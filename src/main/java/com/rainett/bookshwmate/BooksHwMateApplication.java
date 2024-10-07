c66684c0196e34a1faa2746f59ae0076d98f5ba9
rainett <96131229+rainett@users.noreply.github.com>
2024-10-07 17:13:43 +0300

    initial commit

---
+++ src/main/java/com/rainett/bookshwmate/BooksHwMateApplication.java
@@ -0,0 +1,13 @@
+package com.rainett.bookshwmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksHwMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksHwMateApplication.class, args);
+    }
+
+}
